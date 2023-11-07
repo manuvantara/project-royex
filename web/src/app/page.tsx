5f2da97f89325b95a0fae0c8f072e6f36de4771c
dreamqip <v.chuchupak@gmail.com>
2023-11-07 21:22:12 +0100

    feat(web): apply custom shadcn theme

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,10 @@
-import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
+      <h1>
+        Hi there
+      </h1>
     </main>
   );
 }
