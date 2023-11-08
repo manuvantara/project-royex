6d0d592065907946ab53dee3ab29211d56be14f8
dreamqip <v.chuchupak@gmail.com>
2023-11-08 02:37:46 +0100

    feat(web): implement /portfolio page's layout

---
+++ web/src/app/portfolio/page.tsx
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker';
+import Card from '@/components/card';
+import CardChart from '@/components/card-chart';
+import CardMetric from '@/components/card-metric';
+import Report from '@/components/report';
+
+const fakeStats = Array.from({ length: 4 }, (_) => ({
+  title: faker.word.words(3),
+  revenue: Number(faker.finance.amount(0, 100000)),
+  percentage: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+}));
+
+export default function Page() {
+  return (
+    <div className="container">
+      <div className="grid gap-4 px-4 pb-8 pt-8 md:grid-cols-2 md:pt-12 lg:grid-cols-4">
+        {fakeStats.map((stat) => (
+          <Card key={stat.title} {...stat} />
+        ))}
+      </div>
+      <div className="grid gap-4 px-4 pb-8 pt-4 md:grid-cols-2">
+        <CardMetric />
+        <Report />
+      </div>
+      <div className="grid grid-cols-3 gap-4 px-4 pb-8 pt-4">
+        {fakeStats.map((item, index) => (
+          <CardChart key={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
