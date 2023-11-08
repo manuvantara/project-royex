6f8f74a70aa4c29b7ce5e8e1167554d7af1223c4
dreamqip <v.chuchupak@gmail.com>
2023-11-08 17:35:27 +0100

    feat(web): place report chart in card

--- web/src/components/report.tsx
+++ web/src/components/report.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
 
 const data = [
   {
@@ -67,42 +68,50 @@ const data = [
 
 export default function Report() {
   return (
-    <ResponsiveContainer width="100%" height={350}>
-      <BarChart data={data}>
-        <XAxis dataKey="name" stroke="#888888" fontSize={12} tickLine={false} axisLine={false} />
-        <YAxis
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-          tickFormatter={(value) => `$${value}`}
-        />
-        <Tooltip
-          cursor={{ fill: 'hsl(var(--accent))' }}
-          content={({ active, payload }) => {
-            if (active && payload && payload.length) {
-              return (
-                <div className="rounded-lg border bg-background p-2 shadow-sm">
-                  <div className="grid grid-cols-2 gap-2">
-                    <div className="flex flex-col">
-                      <span className="text-[0.70rem] uppercase text-muted-foreground">Average</span>
-                      <span className="font-bold text-muted-foreground">{payload[0].value}</span>
+    <Card>
+      <CardHeader>
+        <CardTitle>Exercise Minutes</CardTitle>
+        <CardDescription>Your excercise minutes are ahead of where you normally are.</CardDescription>
+      </CardHeader>
+      <CardContent className="pb-4">
+        <ResponsiveContainer width="100%" height={350}>
+          <BarChart data={data}>
+            <XAxis dataKey="name" stroke="#888888" fontSize={12} tickLine={false} axisLine={false} />
+            <YAxis
+              stroke="#888888"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+              tickFormatter={(value) => `$${value}`}
+            />
+            <Tooltip
+              cursor={{ fill: 'hsl(var(--accent))' }}
+              content={({ active, payload }) => {
+                if (active && payload && payload.length) {
+                  return (
+                    <div className="rounded-lg border bg-background p-2 shadow-sm">
+                      <div className="grid grid-cols-2 gap-2">
+                        <div className="flex flex-col">
+                          <span className="text-[0.70rem] uppercase text-muted-foreground">Average</span>
+                          <span className="font-bold text-muted-foreground">{payload[0].value}</span>
+                        </div>
+                        <div className="flex flex-col">
+                          <span className="text-[0.70rem] uppercase text-muted-foreground">Today</span>
+                          <span className="font-bold">{payload[1].value}</span>
+                        </div>
+                      </div>
                     </div>
-                    <div className="flex flex-col">
-                      <span className="text-[0.70rem] uppercase text-muted-foreground">Today</span>
-                      <span className="font-bold">{payload[1].value}</span>
-                    </div>
-                  </div>
-                </div>
-              );
-            }
+                  );
+                }
 
-            return null;
-          }}
-        />
-        <Bar dataKey="deposited" fill="hsl(221.2 83.2% 53.3%)" radius={[4, 4, 0, 0]} />
-        <Bar dataKey="reported" fill="#adfa1d" radius={[4, 4, 0, 0]} />
-      </BarChart>
-    </ResponsiveContainer>
+                return null;
+              }}
+            />
+            <Bar dataKey="deposited" fill="hsl(221.2 83.2% 53.3%)" radius={[4, 4, 0, 0]} />
+            <Bar dataKey="reported" fill="#adfa1d" radius={[4, 4, 0, 0]} />
+          </BarChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
   );
 }
