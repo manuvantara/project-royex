32acb9f0fb3d1d8470c482ce333ce6ded0ac8bba
dreamqip <v.chuchupak@gmail.com>
2023-11-09 08:53:20 +0100

    feat(web): implement proposal page

---
+++ web/src/app/royalty-tokens/[royaltyId]/collective/proposals/[proposalId]/components/cast-vote.tsx
@@ -0,0 +1,146 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import { formatEther } from 'viem';
+import { useContractRead, useAccount, useContractWrite, useWaitForTransaction } from 'wagmi';
+import * as z from 'zod';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Separator } from '@/components/ui/separator';
+import { COLLECTIVE_ABI, COLLECTIVE_ADDRESS } from '@/lib/abi/collective';
+import { ROYALTY_TOKEN_ABI, ROYALTY_TOKEN_ADDRESS } from '@/lib/abi/royalty-token';
+
+const FormSchema = z.object({
+  vote: z.enum(['for', 'against', 'abstain'], {
+    required_error: 'You need to select a vote option.',
+  }),
+});
+
+function formatVotes(votes: bigint) {
+  const parsed = formatEther(votes);
+
+  return Intl.NumberFormat('en-US', {
+    compactDisplay: 'short',
+    maximumFractionDigits: 1,
+    notation: 'compact',
+  }).format(Number(parsed));
+}
+
+export default function CastVote({ proposalId }: { proposalId: string }) {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+  const { address, isConnected } = useAccount();
+
+  const { data: votes } = useContractRead({
+    abi: ROYALTY_TOKEN_ABI,
+    address: ROYALTY_TOKEN_ADDRESS,
+    args: [address!],
+    enabled: isConnected && !!address,
+    functionName: 'getVotes',
+  });
+
+  const { write, data } = useContractWrite({
+    abi: COLLECTIVE_ABI,
+    address: COLLECTIVE_ADDRESS,
+    functionName: 'castVote',
+  });
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  useEffect(() => {
+    if (isLoading) {
+      toast.loading('Voting...');
+    }
+
+    if (isSuccess) {
+      toast.success('Voted!');
+    }
+  }, [isLoading, isSuccess]);
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    switch (data.vote) {
+      case 'for':
+        write?.({ args: [BigInt(proposalId), 1] });
+        break;
+      case 'against':
+        write?.({ args: [BigInt(proposalId), -1] });
+        break;
+      case 'abstain':
+        write?.({ args: [BigInt(proposalId), 0] });
+        break;
+    }
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="default">Cast vote</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[625px]">
+        <DialogHeader>
+          <DialogTitle>Cast vote</DialogTitle>
+          <DialogDescription>Please select your vote for the proposal below.</DialogDescription>
+        </DialogHeader>
+        <Separator />
+        <div>Voting power: {formatVotes(votes ?? BigInt(0))}</div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+            <FormField
+              control={form.control}
+              name="vote"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="for" />
+                        </FormControl>
+                        <FormLabel className="font-normal">For</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="against" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Against</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="abstain" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Abstain</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={!form.formState.isValid} type="submit" className="w-fit">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ web/src/app/royalty-tokens/[royaltyId]/collective/proposals/[proposalId]/page.tsx
@@ -0,0 +1,55 @@
+import { ArrowBottomRightIcon, ArrowTopRightIcon, SizeIcon } from '@radix-ui/react-icons';
+import { CollectivesService } from '@/api/requests';
+import { PageHeader, PageHeaderHeading } from '@/components/ui/page-header';
+import CastVote from './components/cast-vote';
+
+export default async function Page({ params }: { params: { royaltyId: string; proposalId: string } }) {
+  const proposal = await CollectivesService.getProposal(params.royaltyId, params.proposalId);
+
+  return (
+    <div className="container relative">
+      <div className="flex h-full flex-col overflow-hidden bg-background px-4 pb-8">
+        <PageHeader className="px-0 pb-8">
+          <PageHeaderHeading>{proposal?.title}</PageHeaderHeading>
+        </PageHeader>
+        <div className="grid h-full items-stretch gap-6 py-6 md:grid-cols-[1fr_350px]">
+          <div className="rounded-xl border p-6">
+            <div
+              className="prose prose-invert max-w-none"
+              dangerouslySetInnerHTML={{ __html: proposal?.description }}
+            />
+          </div>
+          <div className="rounded-xl border p-6 shadow">
+            <h3 className="mb-4 text-lg font-medium">Votes</h3>
+            <div className="grid gap-2">
+              <div className="rounded-xl border bg-card p-4 text-card-foreground shadow">
+                <div className="flex items-center justify-between pb-2">
+                  <span className="text-sm font-medium tracking-tight">For</span>
+                  <ArrowTopRightIcon className="h-6 w-6 text-emerald-500" />
+                </div>
+                <div className="text-xl font-semibold">{proposal.votes.for}</div>
+              </div>
+              <div className="rounded-xl border bg-card p-4 text-card-foreground shadow">
+                <div className="flex items-center justify-between pb-2">
+                  <span className="text-sm font-medium tracking-tight">Against</span>
+                  <ArrowBottomRightIcon className="h-6 w-6 text-destructive" />
+                </div>
+                <div className="text-xl font-semibold">{proposal.votes.against}</div>
+              </div>
+              <div className="rounded-xl border bg-card p-4 text-card-foreground shadow">
+                <div className="flex items-center justify-between pb-2">
+                  <span className="text-sm font-medium tracking-tight">Abstain</span>
+                  <SizeIcon className="h-6 w-6" />
+                </div>
+                <div className="text-xl font-semibold">{proposal.votes.abstain}</div>
+              </div>
+            </div>
+            <div className="mt-6 grid">
+              <CastVote proposalId={params.proposalId} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
