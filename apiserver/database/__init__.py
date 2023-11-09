0f61d8672a27adb0bf615cfa501c7797751dfd0b
Illia Malovanyi <96831202+pantemon@users.noreply.github.com>
2023-11-09 15:23:28 +0000

    feat(database): connect postgresql to fastapi and remove custom openapi config

--- apiserver/database/__init__.py
+++ apiserver/database/__init__.py
@@ -1,4 +1,9 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
 from apiserver.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(url=DATABASE_URL, echo=True)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
