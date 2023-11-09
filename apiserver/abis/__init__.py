2ae7188214fb4a1605d08a00d51c208ba4518410
Illia Malovanyi <96831202+pantemon@users.noreply.github.com>
2023-11-09 04:47:26 +0000

    feat(abis): add otc-market contract abi

---
+++ apiserver/abis/__init__.py
@@ -0,0 +1,13 @@
+import json
+import pathlib
+
+def load_json_as_module(file: pathlib.Path):
+    if not file.is_file() and not file.name.endswith(".json"):
+        raise FileNotFoundError(f"The path {file} is not a json file.")
+
+    with open(file, "r") as file:
+        return json.load(file)
+    
+directory = pathlib.Path(__file__).parent
+    
+OtcMarket = load_json_as_module(directory / "OtcMarket.json")
