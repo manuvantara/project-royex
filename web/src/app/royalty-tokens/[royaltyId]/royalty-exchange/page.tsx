c3f635b9b1fcc4d5ab15af4826802254b60a61c9
dreamqip <v.chuchupak@gmail.com>
2023-11-09 08:53:53 +0100

    feat(web): add price chart to royalty-exchange page

--- web/src/app/royalty-tokens/[royaltyId]/royalty-exchange/page.tsx
+++ web/src/app/royalty-tokens/[royaltyId]/royalty-exchange/page.tsx
@@ -1,14 +1,40 @@
 'use client';
 
+import { ResponsiveContainer, LineChart, Line, Tooltip, XAxis } from 'recharts';
 import { formatEther } from 'viem';
 import { useContractRead } from 'wagmi';
+import { useRoyaltyExchangesServiceGetPrice } from '@/api/queries';
 import Card from '@/components/card';
+import { Card as UICard, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { ROYALTY_EXCHANGE_ABI, ROYALTY_EXCHANGE_ADDRESS } from '@/lib/abi/royalty-exchange';
 import roundUpEther from '@/lib/helpers/round-up-ether';
 import PageLayout from '../components/page-layout';
 import ExchangeForm from './components/exchange-form';
 
-export default function Page() {
+export default function Page({ params }: { params: { royaltyId: string } }) {
+  const { data } = useRoyaltyExchangesServiceGetPrice({ royaltyId: params.royaltyId });
+
+  const priceData = data
+    ? [
+        ...data.recentValuesDataset.map((d) => ({
+          ...d,
+          timestamp: new Date(d.timestamp).toLocaleDateString(undefined, {
+            year: '2-digit',
+            month: 'short',
+            day: 'numeric',
+          }),
+        })),
+        {
+          ...data.current,
+          timestamp: new Date(data.current.timestamp).toLocaleDateString(undefined, {
+            year: '2-digit',
+            month: 'short',
+            day: 'numeric',
+          }),
+        },
+      ]
+    : [];
+
   const royaltyTokenReserve = useContractRead({
     address: ROYALTY_EXCHANGE_ADDRESS,
     abi: ROYALTY_EXCHANGE_ABI,
@@ -38,14 +64,74 @@ export default function Page() {
 
   return (
     <PageLayout contractAddress={ROYALTY_EXCHANGE_ADDRESS}>
-      <div className="flex items-start justify-center gap-4">
-        <div className="w-[400px]">
-          <ExchangeForm />
-        </div>
-        <div className="grid gap-4">
-          {stats.map((stat) => (
-            <Card key={stat.title} {...stat} />
-          ))}
+      <div className="pb-8 pt-8 md:pt-12">
+        <UICard>
+          <CardHeader>
+            <CardTitle>Price history</CardTitle>
+          </CardHeader>
+          <CardContent className="h-[300px]">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                data={priceData}
+                margin={{
+                  top: 5,
+                  right: 10,
+                  left: 10,
+                  bottom: 0,
+                }}
+              >
+                <XAxis
+                  dataKey="timestamp"
+                  stroke="#888888"
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                  interval="preserveStartEnd"
+                />
+                <Tooltip
+                  cursor={{ fill: 'hsl(var(--accent))' }}
+                  content={({ active, payload }) => {
+                    if (active && payload && payload.length) {
+                      return (
+                        <div className="flex rounded-lg border bg-background px-6 py-2 shadow-sm">
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">Price</span>
+                            <span className="font-bold text-muted-foreground">{payload[0].value}</span>
+                          </div>
+                        </div>
+                      );
+                    }
+
+                    return null;
+                  }}
+                />
+                <Line
+                  type="monotone"
+                  strokeWidth={2}
+                  dataKey="value"
+                  activeDot={{
+                    r: 6,
+                    style: { fill: 'hsl(var(--primary))', opacity: 0.25 },
+                  }}
+                  style={
+                    {
+                      stroke: 'hsl(var(--primary))',
+                    } as React.CSSProperties
+                  }
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </CardContent>
+        </UICard>
+        <div className="mt-8 grid grid-cols-2 gap-6 px-20">
+          <div className="max-w-[800px]">
+            <ExchangeForm />
+          </div>
+          <div className="grid gap-4">
+            {stats.map((stat) => (
+              <Card key={stat.title} {...stat} />
+            ))}
+          </div>
         </div>
       </div>
     </PageLayout>
