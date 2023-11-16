7ced0159ddf16f37e27c9053b7609e0f4ab901a5
dreamqip <v.chuchupak@gmail.com>
2023-11-16 23:16:43 +0100

    fix(web/royalty-exchange): adjust font size

--- web/src/app/royalty-tokens/[royaltyId]/royalty-exchange/components/exchanger.tsx
+++ web/src/app/royalty-tokens/[royaltyId]/royalty-exchange/components/exchanger.tsx
@@ -1,3 +1,4 @@
+import Balancer from 'react-wrap-balancer';
 import BuyForm from './buy-form';
 import SellForm from './sell-form';
 import Stats from './stats';
@@ -5,11 +6,13 @@ import Stats from './stats';
 export default function Exchanger() {
   return (
     <div className="rounded-md border p-6">
-      <div className="space-y-1.5 p-6">
-        <h1 className="font-semibold">Royalty Exchange</h1>
+      <div className="space-y-1 p-6">
+        <h3 className="text-2xl font-semibold tracking-tight">Royalty Exchange</h3>
         <p className="max-w-sm text-sm text-muted-foreground">
-          Powered by AMM, Royalty Exchange gives you an opportunity to trade royalty tokens at a price determined by the
-          market rate.
+          <Balancer>
+            Powered by AMM, Royalty Exchange gives you an opportunity to trade royalty tokens at a price determined by
+            the market rate.
+          </Balancer>
         </p>
       </div>
       <div className="mt-8 grid grid-cols-3 gap-6">
