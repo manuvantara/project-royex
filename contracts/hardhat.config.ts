192d299b08938aa284da7d0fa1dedc4cb9b515d8
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-11-18 19:57:01 +0100

    refactor(contracts): extract abi task

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,7 @@
-import { HardhatUserConfig, scope, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
-import { extractAbiToClient, extractAbiToJson, extractAbiToTs } from "./scripts/extract-abi";
+import { HardhatUserConfig } from "hardhat/config";
+import "./tasks/extract-abi";
 
 if (!process.env.PRIVATE_KEY) {
   throw new Error("Set PRIVATE_KEY in a .env file");
@@ -24,17 +24,7 @@ const config: HardhatUserConfig = {
       chainId: 1313161555,
     },
   },
+  defaultNetwork: "aurora_testnet",
 };
-
-const abiScope = scope("extract-abi", "Extracts ABI of the contracts");
-
-abiScope.task("json", "Extracts ABI in a format of json files for each contract to /abi")
-  .setAction(async () => { extractAbiToJson(); });
-
-abiScope.task("ts", "Extracts ABI in a format of json files for each contract to /abi")
-  .setAction(async () => { extractAbiToTs(); });
-
-abiScope.task("client", "Extracts ABI for a client purposes")
-  .setAction(async () => { extractAbiToClient(); });
 
 export default config;
