aafc642d6243804ac5cd25cb3ca83516378a367d
dreamqip <v.chuchupak@gmail.com>
2023-11-19 19:01:49 +0100

    feat(web): wrap card-chart in link

--- web/src/components/card-chart.tsx
+++ web/src/components/card-chart.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { Bar, BarChart, Line, LineChart, ResponsiveContainer } from 'recharts';
 
 import { ValueIndicator } from '@/api/requests';
@@ -17,63 +18,65 @@ export default function CardChart({ royaltyTokenSymbol, price, depositedRoyaltyI
   const royaltyIncomeData = parseChartData(depositedRoyaltyIncome);
 
   return (
-    <Card>
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-normal">{royaltyTokenSymbol}</CardTitle>
-      </CardHeader>
-      <CardContent className="space-y-4">
-        <div>
-          <div className="text-2xl font-bold">${price.current.value}</div>
-          <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-          <div className="h-[80px]">
-            <ResponsiveContainer width="100%" height="100%">
-              <LineChart
-                data={priceData}
-                margin={{
-                  top: 5,
-                  right: 10,
-                  left: 10,
-                  bottom: 0,
-                }}
-              >
-                <Line
-                  type="monotone"
-                  strokeWidth={2}
-                  dataKey="value"
-                  activeDot={{
-                    r: 6,
-                    style: { fill: 'hsl(var(--primary))', opacity: 0.25 },
+    <Link href={`/royalty-tokens/${royaltyTokenSymbol}`}>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-normal">{royaltyTokenSymbol}</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <div>
+            <div className="text-2xl font-bold">${price.current.value}</div>
+            <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+            <div className="h-[80px]">
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                  data={priceData}
+                  margin={{
+                    top: 5,
+                    right: 10,
+                    left: 10,
+                    bottom: 0,
                   }}
-                  style={
-                    {
-                      stroke: 'hsl(var(--primary))',
-                    } as React.CSSProperties
-                  }
-                />
-              </LineChart>
-            </ResponsiveContainer>
+                >
+                  <Line
+                    type="monotone"
+                    strokeWidth={2}
+                    dataKey="value"
+                    activeDot={{
+                      r: 6,
+                      style: { fill: 'hsl(var(--primary))', opacity: 0.25 },
+                    }}
+                    style={
+                      {
+                        stroke: 'hsl(var(--primary))',
+                      } as React.CSSProperties
+                    }
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="text-2xl font-bold">+{depositedRoyaltyIncome.current.value}</div>
-          <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-          <div className="mt-4 h-[80px]">
-            <ResponsiveContainer width="100%" height="100%">
-              <BarChart data={royaltyIncomeData}>
-                <Bar
-                  dataKey="value"
-                  style={
-                    {
-                      fill: 'hsl(var(--primary))',
-                      opacity: 1,
-                    } as React.CSSProperties
-                  }
-                />
-              </BarChart>
-            </ResponsiveContainer>
+          <div>
+            <div className="text-2xl font-bold">+{depositedRoyaltyIncome.current.value}</div>
+            <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+            <div className="mt-4 h-[80px]">
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart data={royaltyIncomeData}>
+                  <Bar
+                    dataKey="value"
+                    style={
+                      {
+                        fill: 'hsl(var(--primary))',
+                        opacity: 1,
+                      } as React.CSSProperties
+                    }
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
           </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
