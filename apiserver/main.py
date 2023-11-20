3c4ad574b9dee6292c3b2e1dd3674a5a6194d11e
dreamqip <v.chuchupak@gmail.com>
2023-11-20 01:21:35 +0100

    feat(apiserver): add cors and remove custom openapi

--- apiserver/main.py
+++ apiserver/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
-from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
 
 from sqlmodel import SQLModel
 
@@ -39,6 +39,20 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(generate_unique_id_function=custom_generate_unique_id, lifespan=lifespan)
 
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(collectives.router, prefix="/collectives", tags=["collectives"])
 
 app.include_router(
@@ -66,32 +80,3 @@ app.include_router(
     prefix="/royalty-payment-pools",
     tags=["royalty-payment-pools"],
 )
-
-
-# def custom_openapi():
-#     if app.openapi_schema:
-#         return app.openapi_schema
-#     openapi_schema = get_openapi(
-#         title="Custom title",
-#         version="2.5.0",
-#         summary="This is a very custom OpenAPI schema",
-#         description="Here's a longer description of the custom **OpenAPI** schema",
-#         routes=app.routes,
-#     )
-#     openapi_schema["info"]["x-logo"] = {
-#         "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
-#     }
-
-#     for path_data in openapi_schema["paths"].values():
-#         for operation in path_data.values():
-#             tag = operation["tags"][0]
-#             operation_id = operation["operationId"]
-#             to_remove = f"{tag}-"
-#             new_operation_id = operation_id[len(to_remove) :]
-#             operation["operationId"] = new_operation_id
-
-#     app.openapi_schema = openapi_schema
-#     return app.openapi_schema
-
-
-# app.openapi = custom_openapi
