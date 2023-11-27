e7588a4c1c3e01aae9238e274b066a2d62ee2f75
dreamqip <v.chuchupak@gmail.com>
2023-11-27 19:42:08 +0100

    fix(web): comment out broken sections on the portfolio page

--- web/src/app/portfolio/page.tsx
+++ web/src/app/portfolio/page.tsx
@@ -1,29 +1,29 @@
 import { faker } from '@faker-js/faker';
-import SectionWrapper from './components/section-wrapper';
 
 import Card from '@/components/card';
 import CardMetric from '@/components/card-metric';
-import Report from '@/components/report';
+import Report from '@/components/charts/report';
+import SectionWrapper from './components/section-wrapper';
 
-const fakeStats = Array.from({ length: 4 }, (_) => ({
-  title: faker.word.words(3),
-  revenue: Number(faker.finance.amount(0, 100000)),
-  percentage: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
-}));
+// const fakeStats = Array.from({ length: 4 }, (_) => ({
+//   title: faker.word.words(3),
+//   revenue: Number(faker.finance.amount(0, 100000)),
+//   percentage: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+// }));
 
 export default function Page() {
   return (
     <div className="container">
       <div className="grid gap-4 px-4 pb-8 pt-8 md:grid-cols-2 md:pt-12 lg:grid-cols-4">
-        {fakeStats.map((stat) => (
+        {/* {fakeStats.map((stat) => (
           <Card key={stat.title} {...stat} />
-        ))}
+        ))} */}
       </div>
       <div className="grid gap-4 px-4 pb-8 pt-4 md:grid-cols-2">
         <CardMetric />
-        <Report />
+        {/* <Report title='Total Reported Royalty Income' /> */}
       </div>
-      <SectionWrapper />
+      {/* <SectionWrapper /> */}
     </div>
   );
 }
